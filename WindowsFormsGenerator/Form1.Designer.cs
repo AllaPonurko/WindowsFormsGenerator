7b49483d5eb1e7bab2bf725360193784707b2abd
Alla Ponurko <ponurkoalla264@gmail.com>
2022-09-05 10:16:12 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsGenerator/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace WindowsFormsGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.listBoxNum = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(337, 45);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Нижняя граница";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Верхняя граница";
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(142, 45);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(100, 20);
+            this.txtMin.TabIndex = 3;
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(142, 100);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(100, 20);
+            this.txtMax.TabIndex = 4;
+            // 
+            // listBoxNum
+            // 
+            this.listBoxNum.FormattingEnabled = true;
+            this.listBoxNum.Location = new System.Drawing.Point(549, 100);
+            this.listBoxNum.Name = "listBoxNum";
+            this.listBoxNum.Size = new System.Drawing.Size(120, 95);
+            this.listBoxNum.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(546, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Список простых чисел";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(337, 92);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(549, 215);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 8;
+            this.btnShow.Text = "Показать";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBoxNum);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Генерация простых чисел";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.ListBox listBoxNum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ WindowsFormsGenerator/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            vs = new List<int>();
+        }
+        
+         static public int end=int.MaxValue;
+         static public List<int> vs;
+        static void Method()
+        {
+            for (int i = 2; i <= end; i++)
+            {
+                bool b = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0 & i % 1 == 0)
+                    {
+                        b = false;
+                    }
+                }
+                if (b) vs.Add(i);
+            }
+            
+        }
+        static ThreadStart Start = new ThreadStart(Method);
+        Thread thread = new Thread(Start );
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            thread.Start();
+            foreach(var item in vs)
+            {
+              listBoxNum.Items.Add(item);
+            }
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            thread.Abort();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            listBoxNum.Items.ToString();
+        }
+    }
+}
